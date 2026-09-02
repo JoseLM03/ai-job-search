440d75d9e34ca406b7ce7e64b31134986b2182b0
Jose Montejo <themysteriousguy53@gmail.com>
2026-09-02 18:38:31 -0400

    feat: add Adzuna job collector

---
+++ src/collectors/adzuna.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+import os  
+import httpx
+
+load_dotenv()
+ADZUNA_APP_ID = os.getenv("ADZUNA_APP_ID")
+ADZUNA_APP_KEY = os.getenv("ADZUNA_APP_KEY")
+
+def get_jobs():
+    response = httpx.get(
+        "https://api.adzuna.com/v1/api/jobs/us/search/1",
+        params={
+            "app_id": ADZUNA_APP_ID,
+            "app_key": ADZUNA_APP_KEY,
+            "results_per_page": 50,
+            "what": "python developer",
+            "where": "Atlanta",
+        },
+    )
+    data = response.json()
+    return data.get("results", [])
+    
+if __name__ == "__main__":
+    jobs = get_jobs()
+    print(len(jobs))
+    print(jobs[0]["title"])
--- test_api.py
+++ test_api.py
@@ -1,3 +1,24 @@
+from dotenv import load_dotenv
 import httpx
+import os
 
-print("httpx works!")
+load_dotenv()
+app_id = os.getenv("ADZUNA_APP_ID")
+app_key = os.getenv("ADZUNA_APP_KEY")
+
+response = httpx.get(
+    "https://api.adzuna.com/v1/api/jobs/us/search/1",
+    params={
+        "app_id": app_id,
+        "app_key": app_key
+        "what": "software engineer",
+        "where": "Atlanta",
+        "results_per_page": 10
+    }
+)
+
+data = response.json()
+print(data["count"])
+
+for job in data["results"]:
+    print(job["title"])
